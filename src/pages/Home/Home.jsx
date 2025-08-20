7b57d47761a6a331ebbefeb850d1d96f54661fc8
Md. Rabiul Islam <rabiul77777888@gmail.com>
2025-08-20 16:53:33 +0600

    job portal project setup, route setup, lottie react and firebase setup, auth provider setup and register.

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../shared/Navbar';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2>This is Home.</h2>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import registerLottieData from "../../assets/lottie/register.json"
+import Lottie from "lottie-react";
+
+const Register = () => {
+
+    const handleRegister = e => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        
+        console.log(email, password);
+
+        createUser(email, password)
+        .then(result =>{
+            console.log(result.user)
+        })
+        .catch(error =>{
+            console.log(error.message);
+        })
+    }
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+         
+          <Lottie className="w-80" animationData={registerLottieData}></Lottie>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+             <h1 className="text-5xl font-bold ml-5 mt-5">Register now!</h1>
+          <div className="card-body">
+            <form onSubmit={handleRegister} className="form">
+              <label className="label">Email</label>
+              <input type="email" className="input"
+              name="email" 
+              placeholder="Email" />
+              <label className="label">Password</label>
+              <input type="password" className="input"
+              name="password"
+               placeholder="Password" />
+              <div>
+                <a className="link link-hover">Forgot password?</a>
+              </div>
+              <button className="btn btn-neutral mt-4">Register</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
