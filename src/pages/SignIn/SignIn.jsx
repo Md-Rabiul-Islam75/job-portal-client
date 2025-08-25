ca8edf0db61fc1011ec2b2188bdd9efa30e58466
Md. Rabiul Islam <rabiul77777888@gmail.com>
2025-08-26 01:01:48 +0600

    create private route and auth redirect

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -3,11 +3,16 @@ import registerLottieData from "../../assets/lottie/register.json"
 import Lottie from "lottie-react";
 import AuthContext from "../../context/AuthContext/AuthContext";
 import SocialLogin from "../shared/SocialLogin";
+import { useLocation, useNavigate } from "react-router-dom";
 
 
 const SignIn = () => {
 
   const {signInUser} = useContext(AuthContext);
+  const location = useLocation();
+  const navigate = useNavigate();
+  console.log('in signIn page',location);
+  const from  = location.state || '/';
 
      const handleSignIn = e => {
         e.preventDefault();
@@ -20,6 +25,7 @@ const SignIn = () => {
         signInUser(email, password)
         .then(result => {
           console.log('sign in', result.user)
+          navigate(from)
         })
         .catch(error => {
           console.log(error);
