200743461001d1b7c05a1fadbc67e4ce0b2b9621
Md. Rabiul Islam <rabiul77777888@gmail.com>
2025-08-27 15:29:31 +0600

    use custom hook and created job application post api

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -2,53 +2,69 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 const JobApply = () => {
-    const id = useParams();
-    console.log(id);
+  const { id } = useParams();
+  console.log(id);
+
+  const {user} = useAuth(); 
 
 
-    const submitJobApplication = e => {
-        e.preventDefault();
+  const submitJobApplication = (e) => {
+    e.preventDefault();
 
-        const form = e.target;
-        const linkedIn = form.linkedIn.value;
+    const form = e.target;
+    const linkedIn = form.linkedIn.value;
 
-        const github = form.github.value;
+    const github = form.github.value;
 
-        const resume = form.resume.value;
+    const resume = form.resume.value;
 
-        console.log('here check: ',linkedIn, github, resume);
-    }
+    console.log("here check: ", linkedIn, github, resume);
+
+    const jobApplication = {
+      job_id: id,
+      applicant_email: user.email,
+      linkedIn,
+      github,
+      resume
+    };
+
+    // Send applicationData to the server
+  };
 
   return (
-    <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left">
-          <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-        </div>
-        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <div className="card-body">
-            <form onSubmit={submitJobApplication} className="form">
-              <label className="label">LinkedIn URL</label>
-              <input type="url" className="input" name="linkedIn" placeholder="LinkedIn URL" />
-              <label className="label">Github URL</label>
-              <input type="url" className="input" 
-              name="github"
-              placeholder="Github URL" />
+    <div className="card bg-base-100 w-full shadow-2xl">
 
-              <label className="label">Resume</label>
-              <input type="url" className="input" 
-              name="resume"
-              placeholder="Resume URL" />
-              
-              <button className="btn btn-neutral mt-4">Apply</button>
-            </form>
-          </div>
-        </div>
+      <h1 className="text-5xl text-center font-bold">
+        Apply Job and Good Luck.
+      </h1>
+
+      <div className="card-body">
+        <form onSubmit={submitJobApplication} className="form space-y-1">
+          <label className="label">LinkedIn URL</label>
+          <input
+            type="url"
+            className="input"
+            name="linkedIn"
+            placeholder="LinkedIn URL"
+          /> <br />
+          <label className="label">Github URL</label>
+          <input
+            type="url"
+            className="input"
+            name="github"
+            placeholder="Github URL"
+          /> <br />
+
+          <label className="label">Resume</label>
+          <input
+            type="url"
+            className="input"
+            name="resume"
+            placeholder="Resume URL"
+          />
+
+          <button className="btn btn-neutral mt-4">Apply</button>
+        </form>
       </div>
     </div>
   );
