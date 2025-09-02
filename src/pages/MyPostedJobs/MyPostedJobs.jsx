082a55313d23234d937c6f40c6afef87a648a136
Md. Rabiul Islam <rabiul77777888@gmail.com>
2025-09-02 16:26:32 +0600

    show my posted jobs and count job application

--- src/pages/MyPostedJobs/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs/MyPostedJobs.jsx
@@ -1,25 +1,55 @@
-import React, { useEffect, useState } from 'react';
-import useAuth from '../../hooks/useAuth';
+import React, { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth";
 
 const MyPostedJobs = () => {
+  const [jobs, setJobs] = useState([]);
 
-    const [jobs, setJobs] = useState([]);
+  const { user } = useAuth();
 
-    const {user} = useAuth();
+  useEffect(() => {
+    fetch(`http://localhost:5000/jobs?email=${user.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setJobs(data);
+      });
+  }, [user.email]);
 
-    useEffect(() =>{
-        fetch(`http://localhost:5000/jobs?email=${user.email}`)
-        .then(res => res.json())
-        .then(data => {
-            setJobs(data);
-        });
-    },[user.email])
+  return (
+    <div>
+      <h2 className="text-3xl">My Posted Jobs: {jobs.length}</h2>
 
-    return (
-        <div>
-            <h2 className="text-3xl">My Posted Jobs: {jobs.length}</h2>
-        </div>
-    );
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Company Name</th>
+              <th>Salary Range</th>
+              <th>Application Count</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+
+            {jobs.map((job, index) => (
+              <tr key={job._id}>
+                <th>{index + 1}</th>
+                <td>{job.title}</td>
+                <td>{job.company}</td>
+                <td>
+                  {job.salaryRange.currency} {job.salaryRange.min} -{" "}
+                  {job.salaryRange.max}
+                </td>
+                <td>{job.applicationCount}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default MyPostedJobs;
